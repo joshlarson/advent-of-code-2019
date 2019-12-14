6be608bb134679f6d441366087a8e03718797b65
Josh Larson <theMutatedShrimp@gmail.com>
2019-12-14 17:31:47 -0600

    Day 1, Part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -5,19 +5,49 @@ defmodule Day1 do
     content
     |> fuel_count_for()
     |> IO.puts()
+
+    content
+    |> total_fuel_for()
+    |> IO.puts()
   end
 
   def fuel_count_for(input) do
     input
+    |> fuel_load_for_all_modules(&fuel_load_for_module/1)
+  end
+
+  def total_fuel_for(input) do
+    input
+    |> fuel_load_for_all_modules(&total_fuel_load_for_module/1)
+  end
+
+  defp fuel_load_for_all_modules(input, fuel_fn) do
+    input
     |> String.split("\n")
     |> Stream.filter(&(&1 != ""))
     |> Stream.map(&parse_integer/1)
-    |> Stream.map(&fuel_load_for_module/1)
+    |> Stream.map(fuel_fn)
     |> Enum.sum()
   end
 
+  defp total_fuel_load_for_module(module_mass) do
+    total_fuel_load_for_module(0, module_mass)
+  end
+
+  defp total_fuel_load_for_module(accounted_for, 0) do
+    accounted_for
+  end
+
+  defp total_fuel_load_for_module(accounted_for, additional) do
+    fuel = fuel_load_for_module(additional)
+    total_fuel_load_for_module(accounted_for + fuel, fuel)
+  end
+
   defp fuel_load_for_module(module_mass) do
-    div(module_mass, 3) - 2
+    max(
+      div(module_mass, 3) - 2,
+      0
+    )
   end
 
   defp parse_integer(str) do
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -14,4 +14,44 @@ defmodule Day1Test do
       assert Day1.fuel_count_for("12\n1969\n") == 656
     end
   end
+
+  describe "total_fuel_for" do
+    test "is the same if the fuel would generate no new fuel requirement" do
+      assert Day1.total_fuel_for("6\n") == 0
+      assert Day1.total_fuel_for("7\n") == 0
+      assert Day1.total_fuel_for("8\n") == 0
+      assert Day1.total_fuel_for("9\n") == 1
+      assert Day1.total_fuel_for("10\n") == 1
+      assert Day1.total_fuel_for("11\n") == 1
+      assert Day1.total_fuel_for("14\n") == 2
+      assert Day1.total_fuel_for("26\n") == 6
+    end
+
+    test "negative fuel is not a thing" do
+      assert Day1.total_fuel_for("3\n") == 0
+    end
+
+    test "fuel is needed to carry more than 8 units of fuel" do
+      assert Day1.total_fuel_for("33\n") == 10
+    end
+
+    test "fuel is needed for fuel that's used to carry fuel" do
+      assert Day1.fuel_count_for("104\n") == 32
+      assert Day1.fuel_count_for("32\n") == 8
+      assert Day1.fuel_count_for("8\n") == 0
+      assert Day1.total_fuel_for("104\n") == 40
+
+      assert Day1.fuel_count_for("105\n") == 33
+      assert Day1.fuel_count_for("33\n") == 9
+      assert Day1.fuel_count_for("9\n") == 1
+      assert Day1.total_fuel_for("105\n") == 43
+    end
+
+    test "examples" do
+      assert Day1.total_fuel_for("14\n") == 2
+      assert Day1.total_fuel_for("1969\n") == 966
+      assert Day1.total_fuel_for("100756\n") == 50346
+      assert Day1.total_fuel_for("1969\n14\n100756\n") == 51314
+    end
+  end
 end
